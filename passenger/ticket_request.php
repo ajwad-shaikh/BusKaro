c188d49fc5500beb8a7e46db4dd5dd7165285184
Ajwad <shaikhajwad10@gmail.com>
2018-11-25 19:36:26 +0530

    Added Book Ticket Functionality

---
+++ passenger/ticket_request.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php
+      require 'dbconn.php';
+      require 'header.php';
+  ?>
+<body>
+	<?php
+    require 'navbar.php';
+    require 'db_init.php';
+    $userID = $_SESSION['UserID'];
+    $bid = $_GET['bid'];
+    $sql_instance="SELECT * FROM buskaro.seat_matrix WHERE Passenger=".$userID."AND (BusDate = CURDATE() OR BusDate = CURDATE() + INTERVAL 1 DAY);";
+    $result = $conn->query($sql_instance);
+    if($result->num_rows < 2)
+    {
+      $sql_instance="SELECT * FROM buskaro.seat_matrix WHERE BID=".$bid." AND Passenger IS NULL;";
+      $result = $conn->query($sql_instance);
+      echo $conn->error;
+      $row = $result->fetch_assoc();
+      $sql_start="SET AUTOCOMMIT = OFF; START TRANSACTION;";
+      $result = $conn->query($sql_start);
+      $sql_entry="UPDATE buskaro.seat_matrix SET Passenger = ".$userID." WHERE BID=".$bid." AND SeatNo=".$row['SeatNo'].";";
+      $sql_seat="UPDATE buskaro.bus_instances SET Capacity = Capacity - 1 WHERE BID=".$bid.";";
+      if(($conn->query($sql_entry) == TRUE)&&($conn->query($sql_seat) == TRUE))
+      {
+        $sql_commit = "COMMIT;";
+        $result = $conn->query($sql_commit);
+        echo 'Ticket Booked Successfully!';
+      }
+      else
+      {
+        echo $conn->error;
+        $sql_rollback = "ROLLBACK;";
+        $result = $conn->query($sql_rollback);
+        echo 'Sorry, there was a problem :(';
+      }
+    }
+    else echo 'Sorry, you have exceeded the daily ticket limit!';
+  ?>
+</body>
---
+++ passenger/view_tickets.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php
+      require 'dbconn.php';
+      require 'header.php';
+  ?>
+<body>
+	<?php
+    require 'navbar.php';
+    require 'db_init.php';
+  ?>
+</body>
