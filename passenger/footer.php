286866adac8f8639f20115c1c100352277bdebf7
edzluhan <ed.zluhan@gmail.com>
2019-10-01 18:27:11 -0300

    add copyleft spam

--- passenger/footer.php
+++ passenger/footer.php
@@ -1,3 +1,3 @@
 <footer class="container-fluid text-right">
-  <h4><b>&copy; 2018 BusKaro.</b> All Rights Reserved.</h4>
+  <h4><b><span class="copyleft">&copy; 2018 BusKaro.</span></b> All Rights Reserved.</h4>
 </footer>
