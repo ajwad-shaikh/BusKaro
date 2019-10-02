2ad0ad11142f043c33ca6d42ff4f45b361f237c8
Bhargavamacha <mbsai29@gmail.com>
2019-10-02 22:36:15 +0530

    Footer is now at the end of the page

--- passenger/footer.php
+++ passenger/footer.php
@@ -1,3 +1,3 @@
-<footer class="container-fluid text-right">
+<footer class="container-fluid text-right" style="position:absolute;bottom:0; width:100%;">
   <h4><b>&copy; 2018 BusKaro.</b> All Rights Reserved.</h4>
 </footer>
