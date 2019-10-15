d754e996f7825b95df9719c85f775a44d0307230
Nura Shariff <shariffnura@gmail.com>
2019-10-15 18:00:59 +0100

    Refactored Connection file to use PDO

--- dbconn.php
+++ dbconn.php
@@ -4,11 +4,15 @@ $dbservername = "localhost";
 $dbusername = "root";
 $dbpassword = "";
 $dbname = "buskaro";
+
 // Create connection
-$conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
-// Check connection
-if (!$conn) {
-    echo "Connected unsuccessfully";
-    die("Connection failed: " . mysqli_connect_error());
-}
+try {
+    $conn = new PDO("mysql:host=$dbservername;dbname=$dbname", $dbusername, $dbpassword);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 ?>
